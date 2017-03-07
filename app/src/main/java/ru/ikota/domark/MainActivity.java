13033067fb1068b77bd3e805aa80c1b0b1bc2876
piffoff <jiamepok2008@yandex.ru>
2017-03-07 12:30:57 +0300

    day1

--- app/src/main/java/ru/ikota/domark/MainActivity.java
+++ app/src/main/java/ru/ikota/domark/MainActivity.java
@@ -1,13 +1,46 @@
 package ru.ikota.domark;
 
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getFragmentManager();
+        runSplash();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void runSplash(){
+        SplashFragment splashFragment = new SplashFragment();
+
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, splashFragment)
+                .addToBackStack(null)
+                .commit();
+
     }
 }
