377b1f9d8f95100af5ce474614740b2ea9936113
piffoff <jiamepok2008@yandex.ru>
2017-03-07 15:31:01 +0300

    day1

--- app/src/main/java/ru/ikota/domark/MainActivity.java
+++ app/src/main/java/ru/ikota/domark/MainActivity.java
@@ -8,11 +8,15 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     FragmentManager fragmentManager;
+    PreferenceHelper preferenceHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PreferenceHelper.getInstance().init(getApplicationContext());
+        preferenceHelper = PreferenceHelper.getInstance();
 
         fragmentManager = getFragmentManager();
         runSplash();
--- app/src/main/java/ru/ikota/domark/PreferenceHelper.java
+++ app/src/main/java/ru/ikota/domark/PreferenceHelper.java
@@ -1,8 +1,38 @@
 package ru.ikota.domark;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.hardware.camera2.params.StreamConfigurationMap;
+
 public class PreferenceHelper {
+    private static final String KEY_SPALSH = "KEY_SPLASH";
     private static PreferenceHelper instance;
+    private Context context;
+    private SharedPreferences sharedPreferences;
 
+    private PreferenceHelper(){
+    }
 
+    public static PreferenceHelper getInstance() {
+        if (instance == null) {
+            instance = new PreferenceHelper();
+        }
+        return instance;
+    }
+
+    public void init(Context context) {
+        this.context = context;
+        sharedPreferences = context.getSharedPreferences("sharedPreferences", context.MODE_PRIVATE);
+    }
+
+    public void putBulean(String key, boolean value){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
+
+    public boolean getBoolean(String key){
+        return sharedPreferences.getBoolean(key, false);
+    }
 }
