d556c699521fdbf170609ff94cadaf4334db5a60
piffoff <jiamepok2008@yandex.ru>
2017-03-10 13:11:34 +0300

    ver3

---
+++ app/src/main/java/ru/ikota/domark/dialog/AddingTaskDialog.java
@@ -0,0 +1,16 @@
+package ru.ikota.domark.dialog;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+
+public class AddingTaskDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+         
+
+        return super.onCreateDialog(savedInstanceState);
+    }
+}
